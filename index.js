4ec93de29e076129a375e474cdc6794482dcea67
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2015-07-19 09:04:01 -0700

    add some unit tests and use yargs better

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 #!/usr/bin/env node
 
-var argv = require('yargs').argv
-
+var argv = require('./lib/cli')
 var findFiles = require('./lib/find')
 var runner = require('./lib/run')
 
 var cwd = argv._.length ? argv._ : [process.cwd()]
-var pattern = argv.pattern || argv.p || '*.tap.js'
-var jobs = argv.jobs || argv.j || 1
-var ignore = ['**/node_modules/**'].concat(argv.i || [], argv.ignore || [])
 
-findFiles(cwd, pattern, ignore, runner(jobs))
+findFiles(cwd, argv.pattern, argv.ignore, runner(argv.jobs))
--- test/unit/index.js
+++ test/unit/index.js
@@ -1,1 +1,12 @@
-console.log('unit')
+var argv = require('../../lib/cli')
+var assert = require('chai').assert
+
+assert.equal(argv.h, argv.help, 'help alias configured')
+assert.equal(argv.j, argv.jobs, 'jobs alias configured')
+assert.equal(argv.p, argv.pattern, 'pattern alias configured')
+assert.equal(argv.i, argv.ignore, 'ignore alias configured')
+
+assert.equal(argv.help, false, 'help default')
+assert.equal(argv.jobs, 1, 'jobs default')
+assert.equal(argv.pattern, '*.tap.js', 'pattern default')
+assert.equal(argv.ignore, '**/node_modules/**', 'ignore default')
