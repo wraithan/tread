bb3ea309462f1ec66fe7856c0315ec142cbfb38f
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2015-07-19 08:04:39 -0700

    actually integration test

--- test/integration/index.js
+++ test/integration/index.js
@@ -1,1 +1,11 @@
-console.log('integration')
+'use strict'
+
+var childProcess = require('child_process')
+
+childProcess.exec('tread', function onBoot (err, stdout, stderr) {
+  if (err) {
+    throw err
+  }
+  console.log(stdout)
+  console.error(stderr)
+})
