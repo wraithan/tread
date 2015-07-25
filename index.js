30e778d8f8ca78d2fcb3b84392e8f41213654719
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2015-07-25 11:43:19 -0700

    refactor index to have reporter in separate file

--- index.js
+++ index.js
@@ -1,57 +1,18 @@
 #!/usr/bin/env node
 
 'use strict'
-var util = require('util')
-var columnify = require('columnify')
-var argv = require('./lib/cli').argv
+
+var cli = require('./lib/cli')
 var findFiles = require('./lib/find')
 var runner = require('./lib/run')
-var Timer = require('./lib/timer')
+var defaultReporter = require('./lib/reporter')
 
-var timer = new Timer(true)
-var cwd = argv._.length ? argv._ : [process.cwd()]
+var argv = cli.argv
+var reporter = defaultReporter()
 
-findFiles(cwd, argv.pattern, argv.ignore, runner(argv, reporter, last))
-
-var failures = 0
-var results = []
-function reporter (error, result) {
-  if (error) throw error
-  var status = 'Pass'
-  if (result.exitCode) {
-    status = 'Fail'
-    failures++
-  } else if (result.exitSignal) {
-    if (result.exitSignal === 'SIGTERM') {
-      status = 'Timeout'
-    } else {
-      status = result.exitSignal
-    }
-    failures++
-  }
-  results.push({
-    filename: result.filename,
-    status: status,
-    duration: util.format('%s ms', result.duration)
-  })
-}
-
-/* eslint-disable no-console */
-function last () {
-  timer.end()
-  console.log(columnify(results, {
-    columns: ['filename', 'status', 'duration']
-  }))
-  console.log(columnify({
-    'Count': util.format('%s tests', results.length),
-    'Duration': util.format('%s ms', timer.durationInMillis()),
-    'Average': util.format(
-      '%s ms',
-      (timer.durationInMillis() / results.length).toFixed(2)
-    )
-  }, {
-    columns: ['stat', 'value']
-  }))
-  process.exit(failures) // eslint-disable-line no-process-exit
-}
-/* eslint-enable no-console */
+findFiles(
+  cli.directories,
+  argv.pattern,
+  argv.ignore,
+  runner(argv, reporter.onTest, reporter.onLast)
+)
