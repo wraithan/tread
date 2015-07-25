9ee8605af644bd9b6489d8b648247730c9569750
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2015-07-25 06:47:40 -0700

    Add assertions to integration tests

--- test/integration/index.js
+++ test/integration/index.js
@@ -1,9 +1,18 @@
 'use strict'
 
 var childProcess = require('child_process')
+var path = require('path')
+var assert = require('chai').assert
 
-childProcess.exec('./index.js', function onBoot (err) {
-  if (err) {
-    throw err
-  }
+var TREAD = path.join(process.cwd(), 'index.js')
+
+var proc = childProcess.fork(TREAD, ['-h'])
+
+proc.on('error', function onError (error) {
+  assert.fail(error)
+})
+
+proc.on('exit', function onExit (code, signal) {
+  assert.equal(code, 0, 'should exit 0')
+  assert.isNull(signal, 'should not have exit signal')
 })
